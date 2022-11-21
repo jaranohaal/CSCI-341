a96814eb99647efb4f1245f138ee8acc49214907
Maksat Faizuldanov <maksat.faizuldanov@nu.edu.kz>
2022-11-21 22:41:08 +0600

    work

--- assignment/settings.py
+++ assignment/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%r%j*icpp^$0ouf=)-wp=+3hmguguq49g4&13v9z26d79dkedb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
